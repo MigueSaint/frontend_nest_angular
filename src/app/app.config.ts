ad37fbb32116d59285fc6d0309ae51423eea98a2
MigueSaint <amg.sosa@yavirac.edu.ec>
2024-11-29 10:00:22 -0500

    avances_sakai

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,6 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  //providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideRouter(routes), provideHttpClient(withFetch()), importProvidersFrom(BrowserModule), importProvidersFrom(BrowserAnimationsModule)]
-  providers: [provideRouter(routes), provideHttpClient(withFetch()), importProvidersFrom(BrowserModule), importProvidersFrom(BrowserAnimationsModule)]    
+  //providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient(withFetch()), importProvidersFrom(BrowserModule), importProvidersFrom(BrowserAnimationsModule)]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), importProvidersFrom(BrowserModule), importProvidersFrom(BrowserAnimationsModule)]    
 };
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormControl,FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -12,21 +14,24 @@ import { FormControl,FormGroup, Validators } from '@angular/forms';
 export class LoginComponent {
 
   private http=inject(HttpClient)
+  //private authservice=inject(AuthService)
+  //private router = inject(Router)
   
   loginForm=new FormGroup({
     email:new FormControl("",[Validators.email, Validators.required]),
     password:new FormControl("", Validators.required)
   })
   funIngresar(){
-    this.http.get("http://jsonplaceholder.typicode.com/todos/1").suscribe(
+    this.http.get("http://jsonplaceholder.typicode.com/todos/1").subscribe(
       (res)=>{
         console.log(res)
+        this.router.navigate(["/admin"])
       },
       (error)=>{
         console.log(error)
       }
     )
-    //alert("Ingresando...")
+    alert("Ingresando...")
   }
 
 }
